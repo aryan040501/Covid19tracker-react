56f196f0478ac72fb27222cc8078bbf9fd655b4a
aryan040501 <aryan040501@gmail.com>
2020-12-28 00:57:03 +0530

    setup basics

--- covid19app/src/Components/Navbar.js
+++ covid19app/src/Components/Navbar.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+import "./Navbar.css";
+
 
 function Navbar() {
     return (
-        <div>
-            
-        </div>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <ul className="nav-menu">
+            <li className="nav-bullet">
+            <Link to="/" className="nav-item">
+                Home
+            </Link>
+            </li>
+          <li className="nav-bullet">
+            <Link to="/Stats" className="nav-item">
+                Stats
+            </Link>
+          </li>
+          <li className="nav-bullet">
+              <Link to="/Analyser" className="nav-item">
+                  Analyser
+              </Link>
+          </li>
+          </ul>
+      </div>
+      </nav>
+      
     )
 }
 
