5591d2b562e5a1f6273b613a040dbe02b8df0c6d
aryan040501 <aryan040501@gmail.com>
2021-01-04 04:34:41 +0530

    almost done with minor errors

--- covid19app/src/index.js
+++ covid19app/src/index.js
@@ -2,6 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios'
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async (country) => {
+
+    let changeableUrl = url;
+
+    if(country){
+        changeableUrl = `${url}/countries/${country}`
+    }
+
+    try {
+        const {data : {confirmed, recovered, deaths,lastUpdate}} = await axios.get(changeableUrl)
+        
+
+        return {confirmed, recovered, deaths,lastUpdate};
+    } catch (error) {
+        
+    }
+}
+
+export const fetchDailyData = async() => {
+    try {
+        const {data} = await axios.get(`${url}/daily`);
+
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate
+        }))
+
+        return modifiedData;
+    } catch (error) {
+        
+    }
+}
+
+export const fetchCountries = async() => {
+    try {
+        const {data: {countries}} = await axios.get(`${url}/countries`)
+
+        return countries.map((country)=> country.name);
+    } catch (error) {
+        
+    }
+}
 
 ReactDOM.render(
     <App /> , document.getElementById('root')
